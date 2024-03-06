d44ec75ee68b5a8061fd2a6e262d618022e14cf3
qianwang <wangqianlf@live.com>
2024-03-06 22:50:33 +0100

    ajout de deux constructeurs, un avec saisie de couleur, l'autre sans

--- Rectangle.cs
+++ Rectangle.cs
@@ -6,14 +6,30 @@ namespace Class_rectangle
 {
   class Rectangle
   {
-    private float longueur;
-    private float largeur;
+    // des attriuts privés
+    private float _longueur;
+    private float _largeur;
     private string _couleur;
 
     // création des propiétés de _couleur
     public string Couleur
     {
-      get { return _couleur;}
+      get { return _couleur; }
+    }
+
+    // deux constructeurs un permet de saisir la couleur, mais pas l'autre.
+    // les Constructeurs permettent d'utiliser des attributs private
+    public Rectangle (float Longueur, float Largeur)
+    {
+      this._longueur = Longueur;
+      this._largeur = Largeur;
+    }
+
+    public Rectangle (float Longueur, float Largeur, string Couleur)
+    {
+      this._longueur = Longueur;
+      this._largeur = Largeur;
+      this._couleur = Couleur;
     }
 
   }
