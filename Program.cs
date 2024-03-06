aa49024436362f2e96499e81af657f8fb7a36260
qianwang <wangqianlf@live.com>
2024-03-06 22:19:18 +0100

    réation de la class Rectangle

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_rectangle
+{
+  class Rectangle
+  {
+    private float longueur;
+    private float largeur;
+  }
+}
