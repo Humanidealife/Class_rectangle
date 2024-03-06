fc4495d42d10c6e040dffd9dbf98d04b2cc42910
qianwang <wangqianlf@live.com>
2024-03-06 23:22:52 +0100

    test sur la class Rectangle, création d'un objet et affiche sa couleur

--- Program.cs
+++ Program.cs
@@ -9,7 +9,12 @@ namespace Class_rectangle
     {
         static void Main(string[] args)
         {
-            Console.ReadLine();
+            // instancier un object de la classe Rectangle
+            Rectangle rec = new Rectangle(4.9f, 8.6f, "violet");
+            Console.WriteLine(rec.Couleur);
+
+
+            Console.ReadKey();
         }
     }
 }
